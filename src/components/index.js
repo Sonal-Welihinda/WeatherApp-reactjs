0fb9a0b199ef78aa7fa7531fb77d31cbdfda6da3
Sonal-Welihinda <106462621+Sonal-Welihinda@users.noreply.github.com>
2023-11-23 21:10:44 +0530

    update addCity file path

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as AddCity } from "./addCity/AddCity1";
+export { default as AddCity } from "./addCity/AddCity";
 export { default as Logo } from "./logo/Logo";
 export { default as WeatherCard } from "./weatherCard/WeatherCard";
